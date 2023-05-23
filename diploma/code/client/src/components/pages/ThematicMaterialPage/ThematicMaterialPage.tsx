909ba19bd975036253a055fbcbdf891cf574d48b
Eduard Sukhavenka <avatar_dj@mail.ru>
2023-05-23 18:26:33 +0300

    [DIPLOMA] - Add thematic-material editing for doctor and content-maker

--- diploma/code/client/src/components/pages/ThematicMaterialPage/ThematicMaterialPage.tsx
+++ diploma/code/client/src/components/pages/ThematicMaterialPage/ThematicMaterialPage.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { ThematicMaterial, User } from "firestore/types/collections.types";
+import {
+  Role,
+  ThematicMaterial,
+  User,
+} from "firestore/types/collections.types";
 import useAsyncEffect from "use-async-effect";
 import { firebaseRepositories } from "firestore/data/repositories";
 import { NotificationManager } from "react-notifications";
@@ -9,16 +13,24 @@ import { useNavigate, useParams } from "react-router-dom";
 import { CardContainer } from "components/ui/CardContainer";
 import Avatar from "@mui/material/Avatar";
 import { getUserFullName } from "firestore/helpers";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Route } from "components/routing";
 import ReactHtmlParser from "react-html-parser";
 import { formatDate } from "helpers/helpers";
+import { EditButton } from "components/ui/EditButton";
+import { useSelector } from "react-redux";
+import { getUserInfoSelector } from "store/selectors";
+import pretty from "pretty";
 
 import "./thematic-material.scss";
+import { deepCopy } from "deep-copy-ts";
+
+const formatContent = (content: string) => pretty(content, { ocd: true });
 
 export const ThematicMaterialPage = () => {
   const navigate = useNavigate();
+  const userInfo = useSelector(getUserInfoSelector);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [thematicMaterial, setThematicMaterial] =
     useState<ThematicMaterial | null>(null);
@@ -55,6 +67,61 @@ export const ThematicMaterialPage = () => {
       setIsLoading(false);
     }
   }, []);
+
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const [newContent, setNewContent] = useState<string>("");
+  const [isFormSaving, setIsFormSaving] = useState<boolean>(false);
+
+  const handleThematicMaterialEdit = () => {
+    setNewContent(formatContent(thematicMaterial?.content as string));
+    setIsEditMode((prev) => !prev);
+  };
+
+  const handleCancel = () => {
+    setNewContent("");
+    setIsEditMode(false);
+  };
+
+  const handleFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setNewContent(value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      setIsFormSaving(true);
+      const newThematicMaterial: ThematicMaterial = {
+        ...deepCopy(thematicMaterial),
+        content: newContent,
+      } as ThematicMaterial;
+      await firebaseRepositories.thematicMaterials.updateDoc(
+        newThematicMaterial
+      );
+      const updatedMaterial =
+        await firebaseRepositories.thematicMaterials.getDocById(
+          thematicMaterialId
+        );
+      if (!updatedMaterial) {
+        throw new Error("Материал не найден");
+      }
+      const newAuthor = await firebaseRepositories.users.getDocById(
+        updatedMaterial.author
+      );
+      if (!newAuthor) {
+        throw new Error("Автор материала не найден");
+      }
+      setThematicMaterial(updatedMaterial);
+      setAuthor(newAuthor);
+    } catch (e) {
+      console.log(e);
+      NotificationManager.error("Сохранение статьи", "Страница статьи");
+    } finally {
+      setNewContent("");
+      setIsEditMode(false);
+      setIsFormSaving(false);
+    }
+  };
 
   return (
     <PageContainer className="thematic-material-page">
@@ -97,7 +164,49 @@ export const ThematicMaterialPage = () => {
             </div>
           </CardContainer>
           <CardContainer className="thematic-material-body">
-            {ReactHtmlParser(thematicMaterial.content)}
+            {!isEditMode &&
+              [Role.DOCTOR, Role.CONTENT_MAKER].includes(userInfo.role) && (
+                <EditButton onClick={handleThematicMaterialEdit} />
+              )}
+            {isEditMode ? (
+              <form className="edit-form" onSubmit={handleSubmit}>
+                <TextField
+                  multiline
+                  label="Текст статьи"
+                  name="content"
+                  value={newContent}
+                  onChange={handleFieldChange}
+                  fullWidth
+                  margin="normal"
+                  disabled={isFormSaving}
+                />
+                <div className="controls">
+                  <Button
+                    variant="outlined"
+                    onClick={handleCancel}
+                    disabled={isFormSaving}
+                  >
+                    Отмена
+                  </Button>
+                  {isFormSaving ? (
+                    <LoadingSpinner style={{ margin: "0", width: "100px" }} />
+                  ) : (
+                    <Button
+                      variant="outlined"
+                      type="submit"
+                      disabled={
+                        formatContent(thematicMaterial.content) ===
+                          formatContent(newContent) || isFormSaving
+                      }
+                    >
+                      Подтвердить
+                    </Button>
+                  )}
+                </div>
+              </form>
+            ) : (
+              ReactHtmlParser(thematicMaterial.content)
+            )}
           </CardContainer>
           {/* todo comments for thematic material page */}
           {/*<CardContainer className="comments" title="Комментарии">*/}
